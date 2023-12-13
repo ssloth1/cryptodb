f2b764e91c8987623b8372ffd7b74047735f9d2e
ssloth1 <jamesbebarski@gmail.com>
2023-12-13 05:10:01 -0500

    Backup

--- concept/cryptodb.sql
+++ concept/cryptodb.sql
@@ -1,22 +1,23 @@
 CREATE DATABASE CryptoDatabase;
 USE CryptoDatabase;
-
+  
 CREATE TABLE Blockchain
-(
+( 
   blockchainID INT NOT NULL,
   BuiltOn_blockchainID INT,
   name VARCHAR(255) NOT NULL,
   launch_date DATE NOT NULL,
   block_time FLOAT,
   consensus_mechanism VARCHAR(255) NOT NULL,
+  smart_contract_support BOOLEAN NOT NULL,
   fork_date DATE,
   fork_type VARCHAR(255),
   PRIMARY KEY (blockchainID),
   FOREIGN KEY (BuiltOn_blockchainID) REFERENCES Blockchain(blockchainID)
 );
-
+  
 CREATE TABLE Cryptocurrency
-(
+( 
   cryptocurrencyID INT NOT NULL,
   name VARCHAR(255) NOT NULL,
   symbol VARCHAR(255) NOT NULL,
@@ -25,9 +26,9 @@ CREATE TABLE Cryptocurrency
   PRIMARY KEY (cryptocurrencyID),
   FOREIGN KEY (blockchainID) REFERENCES Blockchain(blockchainID)
 );
-
+  
 CREATE TABLE Exchange
-(
+( 
   exchangeID INT NOT NULL,
   name VARCHAR(255) NOT NULL,
   fee_structure VARCHAR(255),
@@ -36,9 +37,9 @@ CREATE TABLE Exchange
   publically_traded VARCHAR(255) NOT NULL,
   PRIMARY KEY (exchangeID)
 );
-
+  
 CREATE TABLE Market
-(
+( 
   cryptocurrencyID INT NOT NULL,
   price_usd DECIMAL(15, 10) NOT NULL,
   market_cap DECIMAL(15, 2) NOT NULL,
@@ -51,44 +52,45 @@ CREATE TABLE Market
   PRIMARY KEY (cryptocurrencyID),
   FOREIGN KEY (cryptocurrencyID) REFERENCES Cryptocurrency(cryptocurrencyID)
 );
-
+  
 CREATE TABLE Specification
-(
+( 
   specificationID INT NOT NULL,
   cryptocurrencyID INT NOT NULL,
-  algorithm VARCHAR(255),
+  ALGORITHM VARCHAR(255),
+  main_use_case VARCHAR(255),
   total_supply DECIMAL(65, 2),
   max_supply DECIMAL(65, 2),
   last_update DATE,
   PRIMARY KEY (specificationID),
   FOREIGN KEY (cryptocurrencyID) REFERENCES Cryptocurrency(cryptocurrencyID)
 );
-
+  
 CREATE TABLE Community
-(
+( 
   cryptocurrencyID INT NOT NULL,
   website VARCHAR(255),
   twitter VARCHAR(255),
   discord VARCHAR(255),
   reddit VARCHAR(255),
   whitepaper VARCHAR(255),
-  is_open_source VARCHAR(255),
+  is_open_source BOOLEAN,
   repository_url VARCHAR(255),
   PRIMARY KEY (cryptocurrencyID),
   FOREIGN KEY (cryptocurrencyID) REFERENCES Cryptocurrency(cryptocurrencyID)
 );
-
+  
 CREATE TABLE ListedOn
-(
+( 
   cryptocurrencyID INT NOT NULL,
   exchangeID INT NOT NULL,
   PRIMARY KEY (cryptocurrencyID, exchangeID),
   FOREIGN KEY (cryptocurrencyID) REFERENCES Cryptocurrency(cryptocurrencyID),
   FOREIGN KEY (exchangeID) REFERENCES Exchange(exchangeID)
 );
-
+  
 CREATE TABLE Cryptocurrency_founder
-(
+( 
   cryptocurrencyID INT NOT NULL,
   founder VARCHAR(255) NOT NULL,
   PRIMARY KEY (founder, cryptocurrencyID),
--- concept/main.py
+++ concept/main.py
@@ -1,9 +1,12 @@
 import mysql.connector
 from mysql.connector import Error
+from datetime import datetime
 from scrape_market_data import get_data, update_market, update_specifications
+import time
 
 # List of cryptocurrencies that we want to update, each with it's Coingecko ID and symbol.
 crypto = [
+    
     ('bitcoin', 'BTC'), 
     ('ethereum', 'ETH'), 
     ('ripple', 'XRP'), 
@@ -14,39 +17,67 @@ crypto = [
     ('dogecoin', 'DOGE'),
     ('chainlink', 'LINK'),
     ('matic-network', 'MATIC'),
-    ('shiba-inu', 'SHIB')
+    ('shiba-inu', 'SHIB'),
+    ('avalanche-2', 'AVAX'),
+    ('tron', 'TRX'),
+    ('polkadot', 'DOT'),
+    ('uniswap', 'UNI'),
+    ('bitcoin-cash', 'BCH'),
+    ('monero', 'XMR'),
+    ('ethereum-classic', 'ETC'),
+    ('cosmos', 'ATOM'),
+    ('kaspa', 'KAS'),
+    ('internet-computer', 'ICP'),
+    ('stellar', 'XLM'),
+    ('algorand','ALGO'),
+    ('the-open-network', 'TON'),
+    ('aptos', 'APT'),
+    ('crypto-com-chain', 'CRO'),
+    ('near', 'NEAR'),
+    ('vechain', 'VET'),
+    ('bitcoin-cash-sv', 'BSV'),
+    ('ravencoin', 'RVN')
 ]
 
 def main():
     try:
-        # Connect to the crypto database
-        connection = mysql.connector.connect(host='localhost', database='crypto', user='root')
+        connection = mysql.connector.connect(host='localhost', database='cryptodatabase', user='root', password='4341')
 
         if connection.is_connected():
             print("Connected to crypto database.")
-            
-            # Loop through each cryptocurrency and fetch then update the data. 
-            # Specifically the market table and the specification table. 
+
+            # Initialize a counter for API calls
+            api_call_count = 0
+
             for coin_id, symbol in crypto:
+                start_time = datetime.now()
                 coin_data = get_data(coin_id)
-                print(coin_data)
+                
                 if coin_data:
-                    update_market(connection, symbol, coin_data) # Market Data
-                    update_specifications(connection, symbol, coin_data) # Specification data like Max Supply, Total Supply. 
+                    update_market(connection, symbol, coin_data)
+                    update_specifications(connection, symbol, coin_data)
+                
+                end_time = datetime.now()
+                time_taken = end_time - start_time
+                print(f"Time taken to update {symbol}: {time_taken}")
+
+                # Increment the API call counter
+                api_call_count += 1
+
+                # Check if 20 API calls have been made
+                if api_call_count >= 20:
+                    print("Reached 20 API calls, sleeping for 60 seconds...")
+                    time.sleep(60)  # Sleep for 60 seconds
+                    api_call_count = 0  # Reset the counter
 
     except Error as e:
         print("Error while connecting to MySQL", e)
 
     finally:
         print("Updates complete!")
-
-        # Close the connection to the db after making updates. 
         if connection.is_connected():
             connection.close()
             print("Connection to crypto database is closed.")
 
 if __name__ == '__main__':
-    main()
-        
-        
-        
+    main()   
